36411a7c0d621d2d2e084f2eeb55432e2474b8d9
selbekk <kristofer.selbekk@bekk.no>
2016-06-11 22:37:32 +0200

    Add very basic page styling

--- theme/app/functions.php
+++ theme/app/functions.php
@@ -1,5 +1,9 @@
 <?php
 
+// Features
+add_theme_support( 'post-thumbnails' );
+
+// Add scripts
 function include_theme_resources() {
     wp_enqueue_style('styles.css', get_template_directory_uri() .'/resources/styles.css');
     wp_enqueue_script('bundle.js', get_template_directory_uri() .'/resources/bundle.js');
--- theme/app/page.php
+++ theme/app/page.php
@@ -1,3 +1,26 @@
 <?php get_header(); ?>
 
+<?php while ( have_posts() ) : the_post(); ?>
+
+        <article>
+            <?php if(has_post_thumbnail()) : ?>
+                <div class="featured-image" data-src="<?php the_post_thumbnail_url('large'); ?>">
+                    <div class="wrapper">
+                        <h1 class="page-title"><?php the_title(); ?></h1>
+                    </div>
+                </div>
+            <?php else : ?>
+                <div class="wrapper">
+                    <h1 class="page-title"><?php the_title(); ?></h1>
+                </div>
+            <?php endif; ?>
+            <div class="wrapper">
+                <div class="page-content">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+        </article>
+
+<?php endwhile; ?>
+
 <?php get_footer(); ?>
