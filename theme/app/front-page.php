df76c3467e930a5a56241125af35650c2fc2028f
selbekk <kristofer.selbekk@bekk.no>
2016-06-11 02:16:31 +0200

    Add some basic styling

--- theme/app/front-page.php
+++ theme/app/front-page.php
@@ -1,5 +1,16 @@
 <?php include_once('header.php'); ?>
-<div class="wrapper">
-    <h1>Front page</h1>
-</div>
+<section class="image-section">
+    <div class="image-section-background"
+        style="background-image: url(https://unsplash.it/1000/500/?random)"></div>
+    <div class="image-section-content">
+        <div class="wrapper">
+            <h1 class="hero">
+                Fysioterapi, trening og behandling i Lillestrøm
+            </h1>
+            <div class="button-group mod-centered">
+                <a href="#" class="button mod-large">Bestill time</a>
+            </div>
+        </div>
+    </div>
+</section>
 <?php include_once('footer.php'); ?>
